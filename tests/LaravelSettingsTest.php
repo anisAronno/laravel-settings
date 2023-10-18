729b6fc1c6228479b36d460956abb086d0ae37b2
MD. Anichur Rahaman <38912435+anisAronno@users.noreply.github.com>
2023-10-18 18:55:13 +0600

    Setup Test Environment

--- tests/LaravelSettingsTest.php
+++ tests/LaravelSettingsTest.php
@@ -4,4 +4,8 @@ use Orchestra\Testbench\TestCase as OrchestraTestCase;
 
 class LaravelSettingsTest extends OrchestraTestCase
 {
+    public function test_first_test()
+    {
+        $this->assertEquals(2, 2);
+    }
 }
